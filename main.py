71a379d6b5e15cd65010e9a9bbd6ae707174ae61
Niranjan <niranjankeyas08@gmail.com>
2026-01-27 15:42:10 +0530

    commits from niranjan

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 print("Hand Movement SNN Project Started")
 print("Nandana donkey")
+
+print("Hello Vignesh," \
+" I'm Niranjan and I've requested a pull request to your repository." \
+" Please review and merge it. Thank you!")
